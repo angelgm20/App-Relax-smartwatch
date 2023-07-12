42e647333211f5f66603d1553f8b70c984f3adda
Angel gm <anglgm.ti20@utsjr.edu.mx>
2023-07-12 14:40:39 -0600

    Mensaje del commit

---
+++ lib/screens/ambient_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AmbientWatchFace extends StatelessWidget {
+  const AmbientWatchFace({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    backgroundColor: Colors.black,
+    body: SizedBox.expand(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'FlutterOS',
+                style: TextStyle(color: Colors.blue[600], fontSize: 30),
+              ),
+              const SizedBox(height: 15),
+              const FlutterLogo(size: 60.0),
+              Text(
+              'Jose Angel Garcia Martinez',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+              
+            ],
+          
+          ),
+        ),
+      ); 
+}
